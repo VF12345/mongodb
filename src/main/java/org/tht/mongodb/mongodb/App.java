1ff8eda482879ba53ffcaa1183e515d7173eeb54
VF12345 <972828154@QQ.COM>
2016-03-08 14:10:43 +0800

    t

--- src/main/java/org/tht/mongodb/mongodb/App.java
+++ src/main/java/org/tht/mongodb/mongodb/App.java
@@ -1,8 +1,14 @@
 package org.tht.mongodb.mongodb;
 
 /**
- * Hello world!
- *
+ * 
+ * <p>
+ * Description:这里写描述<br />
+ * </p>
+ * @title App.java
+ * @package org.tht.mongodb.mongodb 
+ * @author tanghaitao
+ * @version 0.1 2016年3月8日
  */
 public class App 
 {
