617a95968d3b3aebd5f36e00008c3bb605b915b4
VF12345 <972828154@QQ.COM>
2016-03-08 14:29:11 +0800

    Update App.java

--- src/main/java/org/tht/mongodb/mongodb/App.java
+++ src/main/java/org/tht/mongodb/mongodb/App.java
@@ -1,19 +1,10 @@
 package org.tht.mongodb.mongodb;
 
-/**
- * 
- * <p>
- * Description:这里写描述<br />
- * </p>
- * @title App.java
- * @package org.tht.mongodb.mongodb 
- * @author tanghaitao
- * @version 0.1 2016年3月8日
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-       
+       int b=1;
     }
 }
